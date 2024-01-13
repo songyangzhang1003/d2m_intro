600b3b7f1b4b2c64fafdd1d8ecfd10dbca44f56f
Songyang Zhang <coco@Songyangs-MacBook-Pro.local>
2024-01-13 16:26:05 -0600

    @ysh627 @nrdowling See updated hello world function. Assignment 4, Songyang Zhang

--- d2m_intro.R
+++ d2m_intro.R
@@ -9,3 +9,23 @@ string <- "abc"
 
 df <- data.frame(
   numm = 100)
+
+#Assginment 4 
+hello_world <- function(name, is_morning){
+  #object assignment 
+  greeting <- ""
+  #determine greeting based on whether it is morning and name 
+  if (is_morning == TRUE && name == "Dr. Dowling") {
+    greeting <- ("Good morning, Dr. Dowling!")
+  } else if (is_morning == FALSE && name == "Dr. Dowling") {
+    greeting <- ("Hello Dr. Dowling!")
+  } else if (is_morning == TRUE && name == "Yvette") {
+    greeting <- ("Sup Yvette")
+  } else {
+    greeting <- ("hello there")
+  }
+  return(greeting)
+}
+    
+  
+  
